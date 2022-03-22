7955ef3875bedd08b31326e3732f765f171e4abd
ChamodhS <chamodhsamaranayake928@gmail.com>
2022-03-22 06:49:02 +0530

    2 activities created ,pending dialog box

--- app/src/main/java/uk/ac/westminster/mobile_cw_w1810008/MainActivity.kt
+++ app/src/main/java/uk/ac/westminster/mobile_cw_w1810008/MainActivity.kt
@@ -1,11 +1,47 @@
 package uk.ac.westminster.mobile_cw_w1810008
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //created the two buttons in the first activity
+        var aboutButton : Button = findViewById(R.id.about)
+
+        var newGameButton : Button = findViewById(R.id.newGame)
+
+
+        aboutButton.setOnClickListener {
+
+
+            println("About");
+
+
+
+
+
+        }
+
+
+
+        newGameButton.setOnClickListener {
+
+
+            var intentNewGame = Intent(this,NewGame::class.java)
+
+            startActivity(intentNewGame)
+
+
+        }
+
+
+
+
+
     }
 }
---
+++ app/src/main/java/uk/ac/westminster/mobile_cw_w1810008/NewGame.kt
@@ -0,0 +1,11 @@
+package uk.ac.westminster.mobile_cw_w1810008
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewGame : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_game)
+    }
+}
